6161b5034899cff6258cb7f29c10f13b3d579876
SaiaIlham <ilhamindra09@mhs.mdp.ac.id>
2023-06-23 17:21:24 +0700

    user.js

--- models/User.js
+++ models/User.js
@@ -1,21 +1,38 @@
 const mongoose = require('mongoose')
+const moment = require ('moment')
 
 const userSchema = mongoose.Schema({
     username: {
         type: String,
         required: true,
-        max: 45
+        max : 45
     },
-    created_date:{
+    password: {
+        type: String,
+        required: true,
+        min : 6,
+        max : 255
+    },
+    created_date: {
         type: Date,
         default: Date.now
     },
-    modified_date:{
+    modified_date: {
         type: Date,
         default: null
     }
 }, {
-    versionKey: false
+    versionKey : false
 })
 
-module.exports = mongoose.model('User', userSchema, 'user')
+userSchema.method('toJSON', function(){
+    const {_id, ...object} = this.toObject()
+    object.id = _id
+    object.created_date = moment(object.created_date).format('DD-MM-YYYY HH:mm:ss')
+    if (object.modified_date != null){
+        object.modified_date = moment(object.modified_date).format('DD-MM-YYYY HH:mm:ss')
+    }   
+    return object
+})
+
+module.exports = mongoose.model('User', userSchema,'user')
